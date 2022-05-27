bd513a755973a2cf2debdca2c7f482847cbdeae6
davidlin6711 <89341114+davidlin6711@users.noreply.github.com>
2022-05-27 18:36:33 +0530

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,28 @@
+<?php
+$course1 = "English III";
+$course2 = "Precalculus";
+$course3 = "Music Theory";
+$course4 = "Bio Technology";
+$course5 = "Principles of Technology I";
+$course6 = "Latin II";
+$course7 = "AP US History";
+$course8 = "Business Computer Information Systems";
+$teacher1 = "Ms.Lapan";
+$teacher2 = "Mr.Gideon";
+$teacher3 = "Mrs.Davis";
+$teacher4 = "Ms.Palmer";
+$teacher5 = "Ms.Garcia";
+$teacher6 = "Mrs.Barnett";
+$teacher7 = "Ms.Johanessen";
+$teacher8 = "Mr.James";
+echo"+-------------------------------------------------------+\n";
+echo"|1|\t\t\t     $course1 | \t$teacher1|\n"; 
+echo"|2|\t\t\t     $course2 |     $teacher2|\n"; 
+echo"|3|\t\t\t    $course3 |     $teacher3|\n"; 
+echo"|4|\t\t\t  $course4 |     $teacher4|\n"; 
+echo"|5|\t      $course5 |     $teacher5|\n"; 
+echo"|6|\t\t\t\t$course6 |   $teacher6|\n"; 
+echo"|7|\t\t\t   $course7 | $teacher7|\n"; 
+echo"|8|$course8 |\t$teacher8|\n";
+echo"+-------------------------------------------------------+\n";
+?>
---
+++ 2.php
@@ -0,0 +1,6 @@
+<?php
+$age = (int)readline('How old are you? ');
+$height = readline('How tall are you? ');
+$weight = (int)readline('Howmuch do you weigh? ');
+echo "So you're $age old, $height tall and $weight heavy.";
+?>
