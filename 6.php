144dca801470dca9753f5b43712b7cb5f64cdd05
davidlin6711 <89341114+davidlin6711@users.noreply.github.com>
2022-05-27 20:16:44 +0530

    Add files via upload

---
+++ 6.php
@@ -0,0 +1,8 @@
+<?php
+echo "\n";
+$first_number = (float)readline("What is your first number?\n");
+$second_number = (float)readline("What is your second number?\n");
+$third_number = (float)readline("What is your third number?\n");
+echo "\n";
+echo "($first_number+$second_number+$third_number) / 2 is... ".($first_number+$second_number+$third_number)/2
+?>
